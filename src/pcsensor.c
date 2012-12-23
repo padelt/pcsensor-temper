b55d5aaa51c8f5721a81e5a4c887c51d30c0b3c7
Philipp Adelt <info@philipp.adelt.net>
2012-12-23 15:56:59 +0100

    Updates license in source

--- src/pcsensor.c
+++ src/pcsensor.c
@@ -1,5 +1,6 @@
 /*
- * pcsensor.c by Juan Carlos Perez (c) 2011 (cray@isp-sl.com)
+ * pcsensor.c by Philipp Adelt (c) 2012 (info@philipp.adelt.net)
+ * based on Juan Carlos Perez (c) 2011 (cray@isp-sl.com)
  * based on Temper.c by Robert Kavaler (c) 2009 (relavak.com)
  * All rights reserved.
  *
@@ -7,15 +8,17 @@
  * or as a standalone program (-DUNIT_TEST). The driver will work with some
  * TEMPer usb devices from RDing (www.PCsensor.com).
  *
+ * This driver works with USB devices presenting ID 0c45:7401.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  * 
- * THIS SOFTWARE IS PROVIDED BY Juan Carlos Perez ''AS IS'' AND ANY
+ * THIS SOFTWARE IS PROVIDED BY Philipp Adelt (and other contributors) ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL Robert kavaler BE LIABLE FOR ANY
+ * DISCLAIMED. IN NO EVENT SHALL Philipp Adelt (or other contributors) BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
